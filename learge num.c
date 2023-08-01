e7cec1bbd7e85a0b11de6acc998fddef5274d02c
asm-zakir <138873940+asm-zakir@users.noreply.github.com>
2023-08-02 00:59:10 +0600

    Update learge num.c

--- learge num.c
+++ learge num.c
@@ -4,11 +4,11 @@ int main()
     int A,B,C;
     printf("Enter three numbers:");
     scanf("%d%d%d",&A,&B,&C);
-    if((A>B>C)||(A>C>B))
+    if((A>=B)&&(A>=C))
     {
         printf("The large number is: %d\n",A);
     }
-    else if((B>A>C)||(B>C>A))
+    else if((B>A)||(B>=C))
     {
         printf("The large number is: %d\n",B);
     }
